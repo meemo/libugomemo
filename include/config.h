d49af64113fd56d07cb92a0947700d503beec9d6
meemo <meemo4556@gmail.com>
2022-08-01 10:11:15 -0500

    Add UGO_MEM{SET|CPY}

--- include/config.h
+++ include/config.h
@@ -2,15 +2,19 @@
 #define LIBUGOMEMO_CONFIG_H
 
 /**
- * If the target toolchain does not have standard C memory management functions then redefine them here.
+ * If the target toolchain does not have standard C memory management functions then they can be redefine here.
  *
- * Remember to include the header file that provides these new functions by replacing line 10.
+ * Remember to include the header file that provides these new functions by replacing the appropriate #include.
  */
 #include <stdlib.h>
 #define UGO_MALLOC    malloc
 #define UGO_REALLOC  realloc
 #define UGO_CALLOC    calloc
 #define UGO_FREE        free
+
+#include <string.h>
+#define UGO_MEMSET    memset
+#define UGO_MEMCPY    memcpy
 
 /**
  * Remove or comment if the target's architecture is not little endian or isn't operating in little endian mode.
@@ -19,7 +23,7 @@
  * unless this is set correctly.
  *
  * Note: complete support is not guaranteed until the library is finished.
- * If support is missing for something required for your target then please create an issue on github.
+ * If support is missing for something required for your project then please create an issue on github.
  */
 #define LITTLE_ENDIAN_
 
