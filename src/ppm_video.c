5ea51c3dafa263bf2fa2d2ff17d3c592bb028bac
Meemo <meemo4556@gmail.com>
2022-10-27 16:50:15 -0500

    PPM thumbnail progress

--- src/ppm_video.c
+++ src/ppm_video.c
@@ -11,16 +11,17 @@
  * Decodes a PPM thumbnail to a stream of RGB8 bytes in output_buffer.
  */
 void PPMDecodeThumbnail(const u8 *buffer, u8 *output_buffer) {
-    u16 x, y;
-    u8 tile_x, tile_y, pixel;
+    u8 tile_x, tile_y, x, y;
 
-    for (tile_x = 0; tile_x < PPM_THUMBNAIL_WIDTH; tile_x++) {
-        for (tile_y = 0; tile_y < PPM_THUMBNAIL_HEIGHT; tile_y++) {
-            for (pixel = 0; pixel < 8; pixel += 2) {
-                x = tile_x + pixel;
-                y = tile_y + pixel;
+    for (tile_y = 0; tile_y < PPM_THUMBNAIL_HEIGHT; tile_y += 8) {
+        for (tile_x = 0; tile_x < PPM_THUMBNAIL_WIDTH; tile_x += 8) {
+            for (y = 0; y < 8; y++) {
+                for (x = 0; x < 8; x += 2) {
+                    memcpy(buffer + (((tile_y + y) * 64 ) + (tile_x + x)),     PPM_THUMBNAIL_PALETTE[*buffer >> 0x4], 3);
+                    memcpy(buffer + (((tile_y + y) * 64 ) + (tile_x + x + 1)), PPM_THUMBNAIL_PALETTE[*buffer  & 0xF], 3);
 
-                output_buffer++;
+                    output_buffer++;
+                }
             }
         }
     }
